75439c2f4465b0b9ed401a345c165b71289ccc2b
Averrous Saloom <averus@users.noreply.github.com>
2021-01-05 12:41:26 +0700

    C#

---
+++ C#/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Diagnostics;
+
+
+public class Example
+{
+    public static void Main()
+    {
+        Stopwatch stopwatch = new Stopwatch();
+
+        stopwatch.Start();
+        for (int i = 0; i < 1000000; i++)
+        {
+            
+        }
+        stopwatch.Stop();
+
+        Console.WriteLine("Elapsed Time is {0} ms", stopwatch.ElapsedMilliseconds);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Penelitian_Kecepatan_Iterasi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
